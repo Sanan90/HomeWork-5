234e5419702dede5b185f94feb951bffd17c5296
sanan90 <san-an@bk.ru>
2020-09-03 02:26:03 +0300

    Hw5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class Main {
+    static final int size = 1000000;
+    static final int h = size / 2;
+
     public static void main(String[] args) {
-        
+        float[] arr = new float[size];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = 1;
+        }
+
+
+        long a = System.currentTimeMillis();
+
+        float[] arr1 = new float[h];
+        float[] arr2 = new float[h];
+
+        System.arraycopy(arr, 0, arr1, 0, h);
+        System.arraycopy(arr, h, arr2, 0, h);
+
+
+
+        Thread arr11 = new Thread(new Potok("arr1", arr1));
+        Thread arr22 = new Thread(new Potok("arr2", arr2));
+
+        arr11.start();
+        arr22.start();
+
+
+
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + (i) / 5) *
+                    Math.cos(0.2f + (i) / 5) * Math.cos(0.4f +
+                    (i) / 2));
+        }
+        System.currentTimeMillis();
+        System.out.println(System.currentTimeMillis() - a);
     }
 }
--- src/Potok.java
+++ src/Potok.java
@@ -1,2 +1,44 @@
-public class Potok {
+public class Potok implements Runnable {
+    private float[] arr;
+    String name;
+
+    public Potok(String name, float[] arr) {
+        this.name = name;
+        this.arr = arr;
+    }
+
+    float[] itog = new float[1000000];
+
+
+    public float[] getArr() {
+        return arr;
+    }
+
+    long a = System.currentTimeMillis();
+
+
+
+    @Override
+    public void run() {
+        if (this.name.equals("arr1")) {
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) *
+                        Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+            System.arraycopy(arr, 0, itog, 0, arr.length);
+        } else if (this.name.equals("arr2")) {
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = (float) (arr[i] * Math.sin(0.2f + (i + arr.length) / 5) *
+                        Math.cos(0.2f + (i + arr.length) / 5) *
+                        Math.cos(0.4f + (i + arr.length) / 2));
+            }
+            System.arraycopy(arr, 0, itog, arr.length, arr.length);
+
+        }
+
+        System.currentTimeMillis();
+        System.out.println(System.currentTimeMillis() - a);
+
+
+    }
 }
